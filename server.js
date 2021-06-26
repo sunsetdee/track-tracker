1d1a12b8b49fb621ee8f24648a22c44c9023f9f9
sunsetdee <andiilii@yahoo.com>
2021-06-25 18:41:57 -0700

    finish step 17

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ require('./config/database');
 require('./config/passport');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var tracksRouter = require('./routes/tracks');
 
 var app = express();
 
@@ -45,7 +45,7 @@ app.use(function(req, res, next) {
 });
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/tracks', tracksRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
